f5b4dc2ced98c8201f96ac41a149b49ed3350440
Alberto Elorza <albertoelorza2002@gmail.com>
2022-07-30 11:05:00 +0200

    BoxServer for linking the box to the server

--- betterbox/core/box.py
+++ betterbox/core/box.py
@@ -1,5 +1,8 @@
 from __future__ import annotations
+from socket import gethostbyname
 from types import FunctionType
+
+from ..networking import Server
 
 def private(func: FunctionType):
     setattr(func, 'private', True)
@@ -14,8 +17,31 @@ class MetaBox(type):
                 data.exposed_functions[attr] = obj
         return data
 
+class BoxServerException(Exception): pass
+
+class BoxServer:
+    def __init__(self, box: Box, port: int) -> None:
+        self.box = box
+        #TODO: Avoid address and family hardcoding
+        self.server = Server(gethostbyname('localhost'), port)
+        self.server.on_connect(self.new_connection)
+        self.server.start(self.message_handle)
+
+    def new_connection(self, client): pass
+
+    def message_handle(self, client, msg): pass
+
 class Box(metaclass=MetaBox):
     __instance: Box = None
+
+    def __init__(self) -> None:
+        self.__server: BoxServer = None
+
+    @private
+    def serve_once(self, port: int):
+        if self.__server:
+            raise BoxServerException('A box can only be served once')
+        self.__server = BoxServer(self, port)
 
     @classmethod
     def instance(cls):
@@ -24,6 +50,7 @@ class Box(metaclass=MetaBox):
         return cls.__instance
 
 def serve_box(port):
-    def serve_box_box(target_box):
+    def serve_box_box(target_box: type[Box]):
+        target_box.instance().serve_once(port)
         return target_box
     return serve_box_box

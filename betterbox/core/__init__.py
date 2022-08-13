d5b40bb0135cc750767bfc7cf612210109b4a3e7
Alberto Elorza <albertoelorza2002@gmail.com>
2022-08-13 17:52:45 +0200

    core init

--- betterbox/core/__init__.py
+++ betterbox/core/__init__.py
@@ -1,2 +1,2 @@
-from .box import Box, serve_box, private
-from .remote_box import RemoteBox, use_box, use_local_box
+from .box import Box, serve_box, serve_box_at, private
+from .remote_box import RemoteBox, use_box, use_know_box, use_local_box

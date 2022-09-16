cb0a723e25922126deabe88a883ac8cf0d5daeae
Alberto Elorza <albertoelorza2002@gmail.com>
2022-09-16 09:49:40 +0200

    updated protocol to avoid wrong packet deserialization, also code is self explanatory and simpler

--- betterbox/networking/protocol.py
+++ betterbox/networking/protocol.py
@@ -1,6 +1,21 @@
-from typing import NewType, Tuple
-import math
+from argparse import Action
+from typing import Literal, NewType, Tuple
 import socket as sck
+
+PROT_SIZE_BYTES = 4
+PROT_SIZE_BYTE_ORDER = 'little'
+
+def max_value(): return (2 ** (8 * PROT_SIZE_BYTES)) - 1
+
+def set_protocol_config(size_bytes: int = PROT_SIZE_BYTES, size_bytes_order: Literal['little', 'big'] = PROT_SIZE_BYTE_ORDER):
+    if size_bytes < 1:
+        raise ValueError('The number of bytes must be at least 1')
+    if size_bytes_order not in ['little', 'big']:
+        raise ValueError("The byte order must be either 'little' or 'big'")
+    global PROT_SIZE_BYTES
+    global PROT_SIZE_BYTE_ORDER
+    PROT_SIZE_BYTES = size_bytes
+    PROT_SIZE_BYTE_ORDER = size_bytes_order
 
 class NetworkProtocolException(Exception): pass
 
@@ -14,18 +29,21 @@ PROT_ACTION_FOUND:  ActionCode = 4
 PROT_ACTION_GRANT:  ActionCode = 5
 PROT_ACTION_DENIE:  ActionCode = 6
 
-def read_socket(socket: sck.socket) -> Tuple[ActionCode, bytes]:
-    action, length = split_status(socket.recv(1)[0])
-    message = socket.recv(length)
-    return action, message
+#The format will be [length of size PROT_SIZE_BYTES][1 byte action][... message determied by length]
+#First part: [length of size PROT_SIZE_BYTES][1 byte action] is called header
+#Last part: [... message determied by length] is called body
 
 def write_socket(socket: sck.socket, action: ActionCode, data: bytes):
-    socket.sendall(bytearray([get_status(action, len(data))]))
-    socket.sendall(data)
+    socket.sendall(serialize_header(len(data), action) + data)
 
-def split_status(status: int) -> Tuple[ActionCode, int]:
-    return status & 15, 2**(status >> 4)
+def serialize_header(length: int, action: ActionCode) -> bytes:
+    return length.to_bytes(PROT_SIZE_BYTES, PROT_SIZE_BYTE_ORDER) + bytearray([action])
 
-def get_status(action: ActionCode, length: int) -> int:
-    exp = int(math.log2(length))+1
-    return (exp << 4) + action
+def read_socket(socket: sck.socket) -> Tuple[ActionCode, bytes]:
+    header = socket.recv(PROT_SIZE_BYTES + 1)
+    length, action = deserialize_header(header)
+    data = socket.recv(length)
+    return action, data
+
+def deserialize_header(header: bytes) -> Tuple[int, Action]:
+    return int.from_bytes(header[:PROT_SIZE_BYTES], PROT_SIZE_BYTE_ORDER), header[PROT_SIZE_BYTES]
--- betterbox/networking/protocol_test.py
+++ betterbox/networking/protocol_test.py
@@ -1,12 +1,16 @@
 from .protocol import *
 
-def test_status():
+def test_headers_serialization():
     action, len = PROT_ACTION_NOTIFY, 400
-    assert split_status(get_status(action, len))[0] == action
-    assert split_status(get_status(action, len))[1] >= len
+    assert deserialize_header(serialize_header(len, action)) == (len, action)
     action, len = PROT_ACTION_PING, 122
-    assert split_status(get_status(action, len))[0] == action
-    assert split_status(get_status(action, len))[1] >= len
+    assert deserialize_header(serialize_header(len, action)) == (len, action)
     action, len = PROT_ACTION_APP, 22006
-    assert split_status(get_status(action, len))[0] == action
-    assert split_status(get_status(action, len))[1] >= len
+    assert deserialize_header(serialize_header(len, action)) == (len, action)
+
+def test_headers_config_update():
+    assert serialize_header(4, 0)[0] == 4 #If byte order is little (the default), at the beginning must be the greatest value
+    set_protocol_config(5, 'big')
+    assert max_value() == (2 ** (8 * 5)) - 1 #Check if the formula is correct
+    assert len(serialize_header(4, 0)) == 6 #Check if the length is size bytes + action byte
+    assert serialize_header(4, 0)[0] == 0 #If byte order is big, at the beginning must be the lowest value

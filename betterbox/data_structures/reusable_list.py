61912722368556eb539a186af0f5044a50cfb465
Alberto Elorza <albertoelorza2002@gmail.com>
2022-09-18 15:10:48 +0200

    updated reusable list's internal behave

--- betterbox/data_structures/reusable_list.py
+++ betterbox/data_structures/reusable_list.py
@@ -9,45 +9,41 @@ class ReusableList:
         self.collection: List[Any] = []
         
     def __len__(self) -> int:
-        length = 0
-        for x in self.collection:
-            if x:
-                length += 1
-        return length
+        return sum([int(bool(x)) for x in self.collection])
 
     def append(self, item: Any) -> MemberId:
         pos, append = self.__find_empty()
         if append: self.collection.append(None)
-        self.collection[pos] = item
-        return self.id(pos)
+        self.collection[pos] = (hash(item), item) #The data is a hash of the item and the item
+        return self.__id(pos)
 
     def __delitem__(self, id: MemberId):
         self.remove(id)
 
     def remove(self, id: MemberId):
-        self.collection[id[0]] = None
+        got = self.collection[id[0]]
+        if got and got[0] == id[1]:
+            self.collection[id[0]] = None
 
     def __getitem__(self, id: MemberId) -> Union[Any, None]:
         return self.get(id)
 
     def get(self, id: MemberId) -> Union[Any, None]:
         got = self.collection[id[0]]
-        if got:
-            if hash(got) == id[1]:
-                return got
-        return None
-
-    def id(self, pos: int) -> Union[MemberId, None]:
-        got = self.collection[pos]
-        if got: return pos, hash(got)
+        if got and got[0] == id[1]:
+            return got[1]
         return None
 
     def __find_empty(self) -> Tuple[int, bool]:
         for p, i in enumerate(self.collection):
-            if i == None: return p, False
+            if not i: return p, False
         return len(self.collection), True
 
     def __iter__(self) -> Iterator[MemberId]:
         for i in range(0, len(self.collection)):
-            if id := self.id(i):
+            if id := self.__id(i):
                 yield id
+
+    def __id(self, pos: int) -> Union[MemberId, None]:
+        if got := self.collection[pos]: return pos, got[0]
+        return None

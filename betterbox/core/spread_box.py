64aef43644e5035984e22438ca9cca68e03ea969
Alberto Elorza <albertoelorza2002@gmail.com>
2022-09-18 15:12:55 +0200

    spread adapter base working implementation

--- betterbox/core/spread_box.py
+++ betterbox/core/spread_box.py
@@ -20,3 +20,27 @@ class SpreadBox(GroupBox):
             promises.append(promise)
 
         return PromiseArray(promises)
+
+class SpreadAdapter:
+    def __init__(self, target: SpreadBox, cumulative: bool = False) -> None:
+        self.__target = target
+        self.__arg_list: List[Tuple[Tuple, Dict]] = []
+        self.__cumulative = cumulative
+
+    def spread(self, fn: Union[str, Callable], use_self: bool = False) -> Promise:
+        returns = self.__target.spread(fn, self.__arg_list, use_self)
+        if not self.__cumulative:
+            self.clear()
+        return returns
+
+    def submit(self, *args, **kwargs):
+        self.__arg_list.append((args, kwargs))
+
+    def clear(self):
+        self.__arg_list = []
+
+    def __enter__(self) -> SpreadAdapter:
+        return self
+  
+    def __exit__(self, *_):
+        pass

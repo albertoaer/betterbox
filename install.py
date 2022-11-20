d6a27858979df26ba7115d45d7b0ae81f7bc7781
Alberto Elorza <albertoelorza2002@gmail.com>
2022-11-20 13:59:05 +0100

    update setup, and include install script

---
+++ install.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+import os
+
+if __name__ == '__main__':
+    os.system('python setup.py install')
--- setup.py
+++ setup.py
@@ -10,12 +10,12 @@ TEST_DEPENDENCIES = [
 
 setup(
     name='betterbox',
-    packages=find_packages(include=['betterbox']),
+    packages=find_packages(include=('betterbox*',)),
     version='0.1.0',
     description='Python distributed services',
     author='Alberto Elorza',
+    author_email='albertoelorza2002@gmail.com',
     license='MIT',
     install_requires=DEPENDENCIES,
-    tests_requires=TEST_DEPENDENCIES,
-    test_suite='tests',
+    tests_requires=TEST_DEPENDENCIES
 )
